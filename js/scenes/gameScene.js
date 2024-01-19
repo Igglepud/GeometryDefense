889c43a112c84c026534d0059acb8dd3bdecbd41
unknown <ian@cpmstar.com>
2024-01-19 12:35:34 -0500

    grid

--- js/scenes/gameScene.js
+++ js/scenes/gameScene.js
@@ -12,7 +12,29 @@ let gameScene = new Phaser.Class({
   },
 
   create: function () {
-   
+    // draw grid
+    for (let i = 0; i < 40; i++) {
+      let ln = scene.add.line(
+        null,
+        null,
+        i * 34,
+        0,
+        i * 34,
+        GAME_HEIGHT,
+        0x2e345b
+      );
+      ln.setOrigin(0)
+      let ln2 = scene.add.line(
+        null,
+        null,
+        0,
+        i * 34,
+        GAME_WIDTH,
+        i * 34,
+        0x2e345b
+      );
+      ln2.setOrigin(0)
+    }
   },
 
   update: function () {
