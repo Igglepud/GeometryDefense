9b94d39bd926f563447abba1bc9fb8155939fd44
Igglepud <iggle@igglepud.com>
2024-01-19 12:23:13 -0600

    added blue triangle

---
+++ js/classes/enemies/blueTriangle.js
@@ -0,0 +1,14 @@
+class BlueTriangle extends Enemy{
+
+    constructor(x = 0, y = 0) {
+        super(scene, x, y);
+
+        this.triangle=scene.add.triangle(x, y, 0, 0, 0, 0, 0, 0, 0x0000ff);
+        this.add(this.triangle);
+        this.triangle.setOrigin(0.5);
+        
+      }
+
+
+
+}
