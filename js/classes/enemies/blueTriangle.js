93294ae994d7c72107f260604349185753af7048
unknown <ian@cpmstar.com>
2024-01-21 09:48:30 -0500

    stuff

--- js/classes/enemies/blueTriangle.js
+++ js/classes/enemies/blueTriangle.js
@@ -3,7 +3,7 @@ class BlueTriangle extends Enemy{
     constructor(x = null, y = null) {
         super(x, y);
         this.speed=250;
-        this.triangle=scene.add.triangle(x, y, -15, 30, 15, 30, 0, 0, 0x0000ff);
+        this.triangle=scene.add.triangle(0, 0, -15, 30, 15, 30, 0, 0, 0x0000ff);
         this.add(this.triangle);
        this.triangle.setOrigin(.5);
 
