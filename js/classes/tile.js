d69efe18c83df3eb3842c0b5684109ad9710cac8
unknown <ian@cpmstar.com>
2024-01-22 09:59:50 -0500

    refactor

--- js/classes/tile.js
+++ js/classes/tile.js
@@ -1,10 +1,37 @@
 class Tile extends Phaser.GameObjects.Container {
   constructor(x, y, gridX, gridY) {
     super(scene, x, y);
+    let that = this
     this.gridX = gridX
     this.gridY = gridY
+    this.path = false
     this.rectangle = scene.add.rectangle(0 ,0, TILE_SIZE, TILE_SIZE, 0x252945)
     this.rectangle.setOrigin(0)
+    this.rectangle.setInteractive()
+
+    // UNCOMMENT TO DRAW MAPS - DO NOT DELETE
+    /*
+    this.rectangle.on("pointerover", function () {
+      if (!that.path) {
+        // this.setFillStyle(0xecc3a0);
+      }
+    }).on("pointerout", function () {
+      if (!that.path) {
+        // this.setFillStyle(0x252945);
+      }
+    }).on("pointerdown", function () {
+        if (!scene.pathDesign) {
+          scene.pathDesign = []
+        }
+        scene.pathDesign.push({
+          x: that.gridX,
+          y: that.gridY
+        })
+        this.setFillStyle(0xecc3a0);
+        console.log(JSON.stringify(scene.pathDesign))
+    });
+    */
+
     this.path = false
     this.add(this.rectangle)
     scene.add.existing(this);
