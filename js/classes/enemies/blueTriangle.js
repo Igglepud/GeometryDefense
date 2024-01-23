b0e63865e4b4c91f1529f0169fb4b7e209ffe16d
Dustin <iggle@igglepud.com>
2024-01-22 20:58:06 -0600

    added mask and rotation

--- js/classes/enemies/blueTriangle.js
+++ js/classes/enemies/blueTriangle.js
@@ -1,21 +1,55 @@
 class BlueTriangle extends Enemy {
   constructor(x = null, y = null) {
     super(x, y);
+    this.acceleration=0;
     this.speed = ENEMYSTATS.triangles.blue.speed;
     this.health = ENEMYSTATS.triangles.blue.health;
     this.resistance = ENEMYSTATS.triangles.blue.resistance;
-
-    this.triangle = scene.add.triangle(0, 0, -15, 30, 15, 30, 0, 0, 0x0000ff);
+    this.graphics=scene.add.graphics();
+    this.graphics.setVisible(false);
+    this.triangle = scene.add.circle(-15, 0,18, 0x0000ff).setIterations(1/3);
+   // this.triangle.setOrigin(0)
     this.add(this.triangle);
-    this.triangle.setOrigin(0.5);
-
+   
+    
     scene.time.addEvent({
       delay: 1000,
       callback: this.takeDamage,
       callbackScope: this,
       repeat:-1,
     });
-
+   
     }
+    move() {
+      scene.tweens.add({
+        targets: this,
+        duration: this.speed,
+        callbackScope: this,
+        x: scene.level.path.curves[this.currentMove].p1.x + 30,
+        y: scene.level.path.curves[this.currentMove].p1.y + 15,
+        acceleration:1,
+        onUpdate() {
+          this.triangle.angle+=this.acceleration;
+          this.graphics.clear();
+          this.graphics.fillStyle(0xffffff, 1);
+          let circle=this.graphics.fillCircle(this.x-15, this.y, 7);
+         
+          let mask =circle.createGeometryMask(this.graphics);
+          this.setMask(mask);
+          mask.setInvertAlpha(true);
+         
+
+
+        },
+        onComplete: function () {
+          this.currentMove++;
+          if (this.currentMove > scene.level.path.curves.length - 1) {
+            this.destroy();
+          } else {
+            this.move();
+          }
+        },
+      });
+    };
   }
 
