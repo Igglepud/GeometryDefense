0ccf02a03dde3325cfeaf8a24eab938795b9d091
Igglepud <iggle@igglepud.com>
2024-01-25 12:32:29 -0600

    formatting

--- js/classes/enemies/blueTriangle.js
+++ js/classes/enemies/blueTriangle.js
@@ -1,55 +1,52 @@
 class BlueTriangle extends Enemy {
   constructor(x = null, y = null) {
     super(x, y);
-    this.acceleration=0;
+    this.acceleration = 0;
     this.speed = ENEMYSTATS.triangles.blue.speed;
     this.health = ENEMYSTATS.triangles.blue.health;
     this.resistance = ENEMYSTATS.triangles.blue.resistance;
-    this.graphics=scene.add.graphics();
+    this.graphics = scene.add.graphics();
     this.graphics.setVisible(false);
-    this.triangle = scene.add.circle(-15, 0,18, 0x0000ff).setIterations(1/3);
-   // this.triangle.setOrigin(0)
+    this.triangle = scene.add.circle(-15, 0, 18, 0x0000ff).setIterations(1 / 3);
+    // this.triangle.setOrigin(0)
     this.add(this.triangle);
-   
-    
+
     // scene.time.addEvent({
     //   delay: 1000,
     //   callback: this.takeDamage,
     //   callbackScope: this,
     //   repeat:-1,
     // });
-   
-    }
-    move() {
-     this.moveTween= scene.tweens.add({
-        targets: this,
-        duration: this.speed,
-        callbackScope: this,
-        x: scene.level.path.curves[this.currentMove].p1.x + 30,
-        y: scene.level.path.curves[this.currentMove].p1.y + 15,
-        acceleration:1,
-        onUpdate() {
-          this.triangle.angle+=this.acceleration;
-          this.graphics.clear();
-          this.graphics.fillStyle(0xffffff, 1);
-          let circle=this.graphics.fillCircle(this.x-15, this.y, 7);
-         
-          let mask =circle.createGeometryMask(this.graphics);
-          this.setMask(mask);
-          mask.setInvertAlpha(true);
-         
-
-
-        },
-        onComplete: function () {
-          this.currentMove++;
-          if (this.currentMove > scene.level.path.curves.length - 1) {
-            this.destroy();
-          } else {
-            this.move();
-          }
-        },
-      });
-    };
   }
+  move() {
+    this.moveTween = scene.tweens.add({
+      targets: this,
+      duration: this.speed,
+      callbackScope: this,
+      x: scene.level.path.curves[this.currentMove].p1.x + 30,
+      y: scene.level.path.curves[this.currentMove].p1.y + 15,
+      acceleration: 1,
+      onUpdate() {
+        this.triangle.angle += this.acceleration;
+        this.graphics.clear();
+        this.graphics.fillStyle(0xffffff, 1);
+        let circle = this.graphics.fillCircle(this.x - 15, this.y, 7);
 
+        let mask = circle.createGeometryMask(this.graphics);
+        this.setMask(mask);
+        mask.setInvertAlpha(true);
+      },
+      onComplete: function () {
+        this.currentMove++;
+        if (this.currentMove == 1) {
+          this.alive = true;
+        }
+        if (this.currentMove > scene.level.path.curves.length - 1) {
+          this.destroy();
+        } else {
+          this.move();
+        }
+      },
+    });
+  }
+}
--- js/classes/towers/basicTower.js
+++ js/classes/towers/basicTower.js
@@ -1,28 +1,34 @@
 class BasicTower extends Tower {
   constructor(tile) {
-    super(tile, 100, 2, 1)
-    this.turret = scene.add.circle(0, 0, TILE_SIZE / 2, 0x6074A1)
-    this.turret.setOrigin(0)
-    this.add(this.turret)
-    this.sendToBack(this.turret)
+    super(tile, 100, 2, 1);
+    this.turret = scene.add.circle(0, 0, TILE_SIZE / 2, 0x6074a1);
+    this.turret.setOrigin(0);
+    this.add(this.turret);
+    this.sendToBack(this.turret);
   }
 
   fire() {
-    let circle = new Phaser.Geom.Circle(this.tile.x + (TILE_SIZE / 2), this.tile.y + (TILE_SIZE / 2), this.range)
-    _.each(scene.enemies.getChildren(), function(enemy) {
-if(enemy.alive){
-
-      if (circle.contains(enemy.x, enemy.y)) {
-        let projectile = scene.projectiles.getNext()
-        if (!projectile) {
-          console.log('oops, no bullets')
-          return
+    let circle = new Phaser.Geom.Circle(
+      this.tile.x + TILE_SIZE / 2,
+      this.tile.y + TILE_SIZE / 2,
+      this.range
+    );
+    _.each(
+      scene.enemies.getChildren(),
+      function (enemy) {
+        if (circle.contains(enemy.x, enemy.y)) {
+          console.log(enemy);
+          if (enemy.alive) {
+            let projectile = scene.projectiles.getNext();
+            if (!projectile) {
+              console.log("oops, no bullets");
+              return;
+            }
+            projectile.spawn(this.tile.x, this.tile.y, enemy);
+            return false;
+          }
         }
-        projectile.spawn(this.tile.x, this.tile.y, enemy)
-        return false
-      }
-    }
-   
-    }.bind(this))
+      }.bind(this)
+    );
   }
 }
