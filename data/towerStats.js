db347e8dfecb84a27428d835938a4cc2c52dc870
Igglepud <iggle@igglepud.com>
2024-02-01 07:41:27 -0600

    data?

--- data/towerStats.js
+++ data/towerStats.js
@@ -3,5 +3,28 @@ basicTower: {
     cost: 20,
     range: 100,
     damage: 1
-  }
+  },
+  stunTower: {
+    level1:{
+    cost: 20,
+    range: 100,
+    damage: 1,
+      refresh: 500,
+    stun:250,
+    },
+    level2: {
+      cost: 30,
+      range: 125,
+      damage: 1,
+      refresh: 400,
+      stun:400,
+    },
+    level3: {
+      cost: 40,
+      range: 150,
+      damage: 1,
+      refresh: 250,
+      stun:500,
+    },
+  },
 }
