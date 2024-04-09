fbddfc921b1b4c9362c168565ac8f753ddbe74b7
Your Name <you@example.com>
2024-04-09 16:17:24 -0400

    header

--- js/classes/ui/header.js
+++ js/classes/ui/header.js
@@ -6,14 +6,7 @@ class Header extends Panel {
     this.waveText = scene.add.text(1125, 36, 'Wave: 1/10', { fontSize: '18px', fill: '#b4b6c1', fontFamily: "font1"});
     this.scoreText = scene.add.text(16, 16, 'Score: 0', { fontSize: '18px', fill: '#b4b6c1', fontFamily: "font1"});
     this.resourcesText = scene.add.text(16, 36, 'Resources: 100', { fontSize: '18px', fill: '#b4b6c1', fontFamily: "font1"});
-    this.add(scene.add.text(320, 16, 'Geometry Defense', { fontSize: '40px', fill: '#b4b6c1', fontFamily: "font1"}));
-
-    // this.text = scene.add.text(275, 200, "", {
-    //   fontFamily: "font1",
-    //   fontSize: "48px",
-    //   fill: "#808080",
-    //   align: "center",
-    // });
+    this.add(scene.add.text(385, 10, 'Geometry Defense', { fontSize: '40px', fill: '#b4b6c1', fontFamily: "font1"}));
     
     this.add(this.livesText);
     this.add(this.scoreText);
