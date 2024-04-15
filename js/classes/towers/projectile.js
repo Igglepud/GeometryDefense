6d72b8e9d7fc22468b9b3fce2a76d422178794f2
Igglepud <iggle@igglepud.com>
2024-04-15 09:24:38 -0500

    depth of projectiles

--- js/classes/towers/projectile.js
+++ js/classes/towers/projectile.js
@@ -1,47 +1,50 @@
 class Projectile extends Phaser.GameObjects.Container {
   constructor() {
     super(scene, -10, -10);
-    this.image = scene.add.circle(0, 0, 5, 0xAAAAAA)
-    scene.physics.add.existing(this)
-    this.image.setOrigin(0)
-    this.add(this.image)
-    scene.add.existing(this)
-
-    this.target = null
-    this.visible = false
-    this.inactive = true
+    this.image = scene.add.circle(0, 0, 5, 0xff0000);//0xaaaaaa
+    scene.physics.add.existing(this);
+    this.image.setOrigin(0);
+    this.add(this.image);
+    scene.add.existing(this);
+    this.depth = 200;
+    this.target = null;
+    this.visible = false;
+    this.inactive = true;
   }
 
   spawn(x, y, enemy, damage, effect = false, duration = false) {
-    this.x = x
-    this.y = y
-    this.target = enemy
-    this.damage = damage
-    this.effect = effect
-    this.duration = duration
-    this.visible = true
-    this.inactive = false
+    this.x = x;
+    this.y = y;
+    this.target = enemy;
+    this.damage = damage;
+    this.effect = effect;
+    this.duration = duration;
+    this.visible = true;
+    this.inactive = false;
   }
 
   despawn() {
-    this.x = -10
-    this.y = -10
-    this.target = null
-    this.visible = false
-    this.inactive = true
+    this.x = -10;
+    this.y = -10;
+    this.target = null;
+    this.visible = false;
+    this.inactive = true;
   }
 
   tick() {
     scene.physics.moveTo(this, this.target.x - 16, this.target.y - 16, 800);
-    let distance = Phaser.Math.Distance.BetweenPoints(this, {x: this.target.x - 16, y: this.target.y - 16});
+    let distance = Phaser.Math.Distance.BetweenPoints(this, {
+      x: this.target.x - 16,
+      y: this.target.y - 16,
+    });
     if (distance < 22) {
       if (this.effect) {
-        console.log(this.effect)
-        this.target.statusEffect(this.effect, this.duration)
+        console.log(this.effect);
+        this.target.statusEffect(this.effect, this.duration);
       }
-      console.log(this.damage)
-      this.target.takeDamage(this.damage)
-      this.despawn()
+      console.log(this.damage);
+      this.target.takeDamage(this.damage);
+      this.despawn();
     }
   }
 }
--- js/classes/towers/tower.js
+++ js/classes/towers/tower.js
@@ -1,53 +1,57 @@
 class Tower extends Phaser.GameObjects.Container {
   constructor(tile, template) {
     super(scene, 0, 0);
-    this.template = TOWER_STATS[template]
-    this.range = this.template.levels[0].range
-    this.cooldownMax = this.template.levels[0].cooldown
-    this.cost = this.template.levels[0].cost
-    this.rangeBubble = scene.add.circle(TILE_SIZE / 2, TILE_SIZE / 2, this.range, 0xFFFFFF)
-    this.rangeBubble.setOrigin(.5)
-    this.rangeBubble.setAlpha(.2)
-    this.rangeBubble.setScale(0)
+    this.template = TOWER_STATS[template];
+    this.range = this.template.levels[0].range;
+    this.cooldownMax = this.template.levels[0].cooldown;
+    this.cost = this.template.levels[0].cost;
+    this.rangeBubble = scene.add.circle(
+      TILE_SIZE / 2,
+      TILE_SIZE / 2,
+      this.range,
+      0xffffff
+    );
+    this.rangeBubble.setOrigin(0.5);
+    this.rangeBubble.setAlpha(0.2);
+    this.rangeBubble.setScale(0);
     this.rangeBubble.setStrokeStyle(4, 0xefc53f);
-    this.add(this.rangeBubble)
+    this.add(this.rangeBubble);
 
-    this.tile = tile    
-    this.level = 1
-    this.cooldown = 0
-    this.selected = false
-    scene.towers.push(this)
-  
+    this.tile = tile;
+    this.level = 1;
+    this.cooldown = 0;
+    this.selected = false;
+    scene.towers.push(this);
   }
 
   tick() {
     if (this.cooldown === 0) {
-      this.cooldown = this.cooldownMax
-      this.fire()
+      this.cooldown = this.cooldownMax;
+      this.fire();
     }
     this.cooldown--;
   }
 
   select() {
-    deselectAll()
-    this.tile.setDepth(DEPTH.selectedTower)
-    this.selected = true
+    deselectAll();
+    this.tile.setDepth(DEPTH.selectedTower);
+    this.selected = true;
     scene.tweens.add({
       targets: this.rangeBubble,
       duration: 100,
       scale: 1,
-      repeat: 0
+      repeat: 0,
     });
   }
 
   deselect() {
-    this.tile.setDepth(DEPTH.tower)
-    this.selected = false
+    this.tile.setDepth(DEPTH.tower);
+    this.selected = false;
     scene.tweens.add({
       targets: this.rangeBubble,
       duration: 100,
       scale: 0,
-      repeat: 0
+      repeat: 0,
     });
   }
 }
